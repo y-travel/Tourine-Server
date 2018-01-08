6e94563374ca29f8c60f05edea1ca68377df3ded
Ali Mirzaei <zoz.zozm@gmail.com>
2018-01-08 18:31:32 +0330

    Add GetDestinations

---
+++ Tourine.ServiceInterfaces/DestinationService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack;
+using Tourine.Models;
+
+namespace Tourine.ServiceInterfaces
+{
+    public class DestinationService : AppService
+    {
+        public IAutoQueryDb AutoQuery { get; set; }
+
+        public object Get(GetDestinations destReq)
+        {
+            var query = AutoQuery.CreateQuery(destReq, Request.GetRequestParams());
+            return AutoQuery.Execute(destReq, query);
+        }
+    }
+}
---
+++ Tourine.Test/DestinationServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using ServiceStack.Messaging.Rcon;
+using ServiceStack.OrmLite;
+using Tourine.Models;
+
+namespace Tourine.Test
+{
+    public class DestinationServiceTest : ServiceTest
+    {
+        private Guid _testDestGuid = Guid.NewGuid();
+        [SetUp]
+        public new void Setup()
+        {
+            createDestination();
+        }
+
+        [Test]
+        public void GetDestinations_should_return_result()
+        {
+            var res = Client.Get(new GetDestinations());
+            res.Results.Count.Should().Be(1);
+        }
+
+        public void createDestination()
+        {
+            Db.Insert(new Destination {Id = _testDestGuid, Name = "Karbala"});
+        }
+    }
+}

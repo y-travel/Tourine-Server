1787bda985ac23c340bb4d5363722de21eea7c82
Ali Mirzaei <zoz.zozm@gmail.com>
2018-01-08 18:50:42 +0330

    Add Reagent and Customer to DataBase and Model

---
+++ Tourine.Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourine.Models
+{
+    public class Customer
+    {
+        public Guid Id { set; get; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string MobileNumber { get; set; }
+        public string NationalCode { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime PassportExpireDate { get; set; }
+        public string PassportNumber { get; set; }
+    }
+}
---
+++ Tourine.Models/Reagent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourine.Models
+{
+    public class Reagent
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string AgencyName { get; set; }
+        public string MobileNo { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}

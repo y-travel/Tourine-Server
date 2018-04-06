578c3c3bde7fe5259d2e1f25163b7529fcc3328c
AiMrz <zoz.zozm@gmai.com>
2018-04-06 20:22:49 +0430

    #394 Bug - Buyer can be infant or Under5

--- Tourine.ServiceInterfaces/Teams/UpsertTeamValidator.cs
+++ Tourine.ServiceInterfaces/Teams/UpsertTeamValidator.cs
@@ -9,6 +9,8 @@ namespace Tourine.ServiceInterfaces.Teams
             RuleFor(t => t.TourId).NotEmpty().NotNull();
             RuleFor(t => t.Buyer.PersonId).NotNull().NotEmpty();
             RuleFor(t => t.Buyer.PersonIncomes.Count).Equal(3);
+            RuleFor(t => t.Buyer.Person.IsInfant).NotEqual(true);
+            RuleFor(t => t.Buyer.Person.IsUnder5).NotEqual(true);
         }
     }
 }

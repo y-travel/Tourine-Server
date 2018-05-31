05b27a9faa647b8477f3eeddca50964a84724e19
AliMrz <zoz.zozm@outlook.com>
2018-05-31 19:57:26 +0430

    #485 - change download excel template api

--- Tourine.ServiceInterfaces/Download/DownloadService.cs
+++ Tourine.ServiceInterfaces/Download/DownloadService.cs
@@ -9,13 +9,15 @@ namespace Tourine.ServiceInterfaces.Download
         public object Get(GetTicketReportTemplate req)
         {
             var fileFullPath = $@"{PublicExtensions.GetRootDirectory()}\bin\tourinetemplate1.xlsx";
-            const string mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
-            var fi = new FileInfo(fileFullPath);
-
-            var reportBytes = File.ReadAllBytes(fi.FullName);
-            var result = new HttpResult(reportBytes, mimeType);
-            result.Headers.Add("Content-Disposition", "attachment; filename=\"" + "ticketReport" + ".xlsx\";");
-            return result;
+//            const string mimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+//            var fi = new FileInfo(fileFullPath);
+//
+//            var reportBytes = File.ReadAllBytes(fi.FullName);
+//            var result = new HttpResult(reportBytes, mimeType);
+//            result.Headers.Add("Content-Disposition", "attachment; filename=\"" + "ticketReport" + ".xlsx\";");
+//            return result;
+            var fs = new FileStream(fileFullPath, FileMode.Open);
+            return new FileStreamResult(fs);
         }
     }
 }
--- Tourine.ServiceInterfaces/Download/GetTicketReportTemplate.cs
+++ Tourine.ServiceInterfaces/Download/GetTicketReportTemplate.cs
@@ -10,28 +10,28 @@ namespace Tourine.ServiceInterfaces.Download
     {
     }
 
-//    public class FileStreamResult : IHasOptions, IStreamWriter
-//    {
-//        private readonly Stream _responseStream;
-//        public IDictionary<string, string> Options { get; }
-//
-//        public FileStreamResult(Stream responseStream, string fileName)
-//        {
-//            _responseStream = responseStream;
-//            Options = new Dictionary<string, string>
-//            {
-//                {"Content-Type","application/octet-stream" },
-//                {"Content-Disposition","attachment; filename=\"" + fileName + "\";" }
-//            };
-//        }
-//
-//        public void WriteTo(Stream responseStream)
-//        {
-//            if ( _responseStream == null)
-//                return;
-//            _responseStream.WriteTo(responseStream);
-//            responseStream.Flush();
-//            responseStream.Dispose();
-//        }
-//    }
+    public class FileStreamResult : IHasOptions, IStreamWriter
+    {
+        private readonly Stream _responseStream;
+        public IDictionary<string, string> Options { get; }
+
+        public FileStreamResult(Stream responseStream)
+        {
+            _responseStream = responseStream;
+            Options = new Dictionary<string, string>
+            {
+                {"Content-Type","application/octet-stream" },
+                {"Content-Disposition","attachment; filename=\"" + "ticketReport" + ".xlsx\";" }
+            };
+        }
+
+        public void WriteTo(Stream responseStream)
+        {
+            if ( _responseStream == null)
+                return;
+            _responseStream.WriteTo(responseStream);
+            responseStream.Flush();
+            responseStream.Dispose();
+        }
+    }
 }
